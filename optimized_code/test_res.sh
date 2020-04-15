785530780e349a5463c9def71ed171ccbefb942e
Avaquet <alan.vaquet@gmail.com>
2020-04-15 22:00:17 +0200

    script pour tester les images

---
+++ optimized_code/test_res.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in {0..319}
+do
+   ./display --checksum output/resultat.raw $i >> output/check_resultat.txt
+   ./display --checksum output/mire.raw $i >> output/check_mire.txt
+done
+
+diff output/check_resultat.txt output/check_mire.txt
